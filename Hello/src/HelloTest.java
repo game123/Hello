22805498b0538b234286fa16949b49e6dbbebcda
VMWARE <VMWARE@192.168.133.132>
2012-07-31 09:59:18 +0800

    Second line

--- Hello/src/HelloTest.java
+++ Hello/src/HelloTest.java
@@ -7,6 +7,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		System.out.println("This is the second line.");
 
 	}
 
